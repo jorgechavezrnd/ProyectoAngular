234851c76f960ffef41bf8b1ef0b0a52840c9d50
jorgechavezrnd <jorgechavezrnd@gmail.com>
2017-09-12 13:56:12 -0400

    Tarea

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarraNavegacionComponent } from './barra-navegacion/barra-navegacion.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarraNavegacionComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/barra-navegacion/barra-navegacion.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'barraNavegacion',
+  templateUrl: './barra-navegacion.component.html',
+  styleUrls: ['./barra-navegacion.component.css']
+})
+export class BarraNavegacionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
